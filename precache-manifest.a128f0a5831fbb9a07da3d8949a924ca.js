f037454170dc49225e24654d5411ac1904c06cb2
Vladimir <bobuzh@gmail.com>
2019-12-05 16:39:06 +0200

    Updates

---
+++ precache-manifest.a128f0a5831fbb9a07da3d8949a924ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65372532683c5d723a841995f62d95d",
+    "url": "/worm/index.html"
+  },
+  {
+    "revision": "bc8b79f9a9ae4e6adfa5",
+    "url": "/worm/static/css/main.b2aeb319.chunk.css"
+  },
+  {
+    "revision": "6e27a2d26039e8134b87",
+    "url": "/worm/static/js/2.0e0d6525.chunk.js"
+  },
+  {
+    "revision": "bc8b79f9a9ae4e6adfa5",
+    "url": "/worm/static/js/main.25ff54bd.chunk.js"
+  },
+  {
+    "revision": "4470209d9cbf28308757",
+    "url": "/worm/static/js/runtime-main.90401aaa.js"
+  }
+]);
